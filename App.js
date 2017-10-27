45ea0acac3fbfb76c123c7fcc91955ecba1e0575
ellipe <ellipe@gmail.com>
2017-10-27 11:39:31 -0500

    First commit

---
+++ App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React from 'react';
+import Header from './src/components/Header';
+
+const App = () => {
+  return (
+      <Header />
+    );
+};
+
+export default App;
